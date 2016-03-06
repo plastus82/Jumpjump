a9ec1dcb84b0af8245e76697bf243618defda828
plastus82 <plastus82@gmail.com>
2016-03-06 11:37:45 +0100

    End of last 9th video
    all sound effect are on, no menu

--- Assets/Scripts/BunyController.cs
+++ Assets/Scripts/BunyController.cs
@@ -1,37 +1,93 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BunyController : MonoBehaviour {
 
     private Rigidbody2D myRigidBody;
     private Animator myAnim;
+    private BoxCollider2D myBunnyCollider;
+    private float bunnyhurtTime=-1;
+    private float startTime;
+    private int jumpsleft = 2;
 
-    public float bunnyJunmpForce = 500f;
+    public Text scoreText;
+    public float bunnyJunmpForce = 700f;
+    public AudioSource jumpSFx;
+    public AudioSource deathSFx;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start ()
+    {
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnim = GetComponent<Animator>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Input.GetButtonUp("Jump"))
-        {
-            myRigidBody.AddForce(transform.up * bunnyJunmpForce);
+        myBunnyCollider = GetComponent<BoxCollider2D>();
+
+        startTime = Time.time;
+    }
+
+// Update is called once per frame
+void Update()
+    {
+        if (bunnyhurtTime == -1)
+        { 
+            if (Input.GetButtonUp("Jump") && jumpsleft > 0)
+            {
+                if(myRigidBody.velocity.y <0)
+                {
+                    myRigidBody.velocity = Vector2.zero ;
+                }
+
+                if (jumpsleft == 1)
+                {
+                    myRigidBody.AddForce(transform.up * bunnyJunmpForce * 0.7f);
+                }
+                else
+                {
+                    myRigidBody.AddForce(transform.up * bunnyJunmpForce);
+                }
+                jumpsleft--;
+                jumpSFx.Play();
+            }
+            myAnim.SetFloat("vVelocity", myRigidBody.velocity.y);
+            scoreText.text = (Time.time - startTime).ToString("0.0");
         }
-        myAnim.SetFloat("vVelocity",myRigidBody.velocity.y);
+        else
+        {
+            if (Time.time > bunnyhurtTime + 2)
+            {
+                SceneManager.LoadScene("GameScene");
+            }
+        }
 	}
 
     //Collision detection with cactus
     void OnCollisionEnter2D(Collision2D collision)
     {
-        // Application.LoadLevel(Application.loadedLevel);
-        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))       
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            SceneManager.LoadScene("GameScene");
+            foreach (MoveLeft moveleLter in FindObjectsOfType<MoveLeft>())
+            {
+                moveleLter.enabled = false;
+            }
+            
+            foreach (PrefabSpawner spawner in FindObjectsOfType<PrefabSpawner>())
+            {
+                spawner.enabled = false;
+            }  
+
+            bunnyhurtTime = Time.time;
+            myAnim.SetBool("bunnyHurt", true);
+            myRigidBody.velocity = Vector3.zero;
+            myRigidBody.AddForce(transform.up * bunnyJunmpForce);
+            myBunnyCollider.enabled = false;         
+            deathSFx.Play();
         }
 
+        if (collision.collider.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            jumpsleft = 2;
+        }
     }
 }
--- Assets/Scripts/PrefabSpawner.cs
+++ Assets/Scripts/PrefabSpawner.cs
@@ -6,21 +6,40 @@ public class PrefabSpawner : MonoBehaviour {
     private float nextSpawn = 0;
 
     public Transform prefabToSpawn;
-    public float spawnRate = 1;
-    public float randomDelay = 1;
+    //public float spawnRate = 1;
+    public float randomDelay = 0.25f;
+    public AnimationCurve spawnCurve;
+    public float curveLengthsinSeconds = 30f;
+    private float startTime;
+    public float randomPositionY = 0;
+    public bool canSpawn = true;
+    
 
 	// Use this for initialization
 	void Start () {
-	
+        startTime = Time.time;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
-        if(Time.time>nextSpawn)
+
+        if (Time.time > nextSpawn && canSpawn)
         {
-            Instantiate(prefabToSpawn, transform.position, Quaternion.identity);
-            nextSpawn = Time.time + spawnRate + Random.Range(0, randomDelay);
+            float moveY = Random.Range(-randomPositionY, randomPositionY);
+
+            Instantiate(prefabToSpawn, transform.position + new Vector3(0, moveY, 0), Quaternion.identity);
+           // nextSpawn = Time.time + spawnRate + Random.Range(0, randomDelay);
+
+
+            float curvePosition = (Time.time - startTime) / curveLengthsinSeconds;
+            if(curvePosition >1f)
+            {
+                curvePosition = 1f;
+                startTime = Time.time;
+
+            }
+
+            nextSpawn = Time.time + spawnCurve.Evaluate(curvePosition) + Random.Range(-randomDelay,randomDelay);
         }
 	}
 }
